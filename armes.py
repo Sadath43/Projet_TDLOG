11a05e14e8852570552fa0c6493d8198dda946bf
SADATH DIESSONGO <sadath.diessongo@centrale-casablanca.ma>
2022-10-12 21:06:36 +0100

    "ajout de classe lance missile anti surface"

--- armes.py
+++ armes.py
@@ -1,6 +1,6 @@
 
 class Weapons:
-    def __int__(self,munition,range):
+    def __init__(self,munition,range):
         self.munition=munition
         self.range=range
 
@@ -9,3 +9,9 @@ class Weapons:
             print("NoAmunitionError")
         else munition-=1
 
+class Lance_missiles_antisurface(Weapons):
+    def __init__(self):
+        super().__init__(self,munition,range):
+    def fire_at(self,x,y,z):
+        if z==0 || np.sprt(x**2+y**2+z**2)>30:
+            print("OutOfRangeError")
