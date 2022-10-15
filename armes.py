78c5754cd4b536ad8c16795929fa9eedf1ff7950
Sadath43 <sadath.diessongo@centrale-casablanca.ma>
2022-10-15 23:30:34 +0100

    mise à jour armes

--- armes.py
+++ armes.py
@@ -1,17 +1,25 @@
-
+import numpy as np
 class Weapons:
     def __init__(self,munition,range):
-        self.munition=munition
-        self.range=range
+        self._munition=munition
+        self._range=range
 
     def fire_at(self,x,y,z):
-        if munition==0:
+        if _munition==0:
             print("NoAmunitionError")
-        else munition-=1
+        else:
+             _munition=_minition-1
 
+#implementation des armes:
+Lance_missiles_antisurface=Weapons(40,30)
+
+"""
 class Lance_missiles_antisurface(Weapons):
     def __init__(self):
         super().__init__(self,munition,range):
     def fire_at(self,x,y,z):
-        if z==0 || np.sprt(x**2+y**2+z**2)>30:
+        if z==0 or np.sprt(x**2+y**2+z**2)>30:
             print("OutOfRangeError")
+"""
+
+
